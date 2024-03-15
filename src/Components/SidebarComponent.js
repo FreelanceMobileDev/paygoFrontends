70603b6b906e88a108c44b7d87a8bfdcc9f8d5dd
kumar@kashish <mobiledevelopers74@gmail.com>
2024-03-15 20:30:58 +0530

    chnage

--- src/Components/SidebarComponent.js
+++ src/Components/SidebarComponent.js
@@ -10,44 +10,44 @@ const SidebarComponent = () => {
                 <div className="main">
                     <ul className="menu">
                         <li>
-                            <Link to="/home">
+                            
                                 <span> Home</span>
-                            </Link>
+                            
                         </li>
                         <li>
-                            <Link to="/user">
+                           
                                 <span> Users</span>
-                            </Link>
+                            
                         </li>
                         <li>
-                            <Link to="/messages">
+                            
                                 <span> User Request</span>
-                            </Link>
+                            
                         </li>
                         <li>
-                            <Link to="/jobs">
+                            
                                 <span> Messages</span>
-                            </Link>
+                            
                         </li>
                         <li>
-                            <Link to="/completed-jobs">
+                            
                                 <span> Insurance Loan</span>
-                            </Link>
+                            
                         </li>
                         <li>
-                            <Link to="/support">
+                            
                                 <span> Financial Loan</span>
-                            </Link>
+                            
                         </li>
                         <li>
-                            <Link to="/settings">
+                            
                                 <span> Add agent</span>
-                            </Link>
+                            
                         </li>    
                         <li>
-                            <Link to="/settings">
+                            
                                 <span> Premium Information</span>
-                            </Link>
+                            
                         </li>                     
                     </ul>
                 </div>
