76bdf50abb8900000f0eafd61a596480d9222d58
phpdev047 <phpdev047@gmail.com>
2024-04-01 12:42:44 +0530

    change

--- src/Components/DashboardComponent.js
+++ src/Components/DashboardComponent.js
@@ -8,29 +8,29 @@ const DashboardComponent = () => {
         <div className="dashboard">
           <h1>Dashboard</h1>
           <div class="dashboard-menu-container">
-          <div className="dashboard-menu">
-                <div className="dashboard-menu-box">
-                   <h1> User Approval Request </h1> 
-                </div>
-                <div className="dashboard-menu-box">
-                    <h1> Loan Approval Request </h1>
-                </div>
-                <div className="dashboard-menu-box">
-                    <h1> Total insurance</h1>
-                </div>
-                <div className="dashboard-menu-box">
-                    <h1> Total Loan Approval</h1>
-                </div>
-                <div className="dashboard-menu-box">
-                    <h1> Total Enquiry</h1>
-                </div>
-                <div className="dashboard-menu-box">
-                    <h1> Total Car Insurance</h1>
-                </div>
-                <div className="dashboard-menu-box">
-                    <h1> Total Cars</h1>
-                </div>
-          </div>
+            <div className="dashboard-menu">
+              <div className="dashboard-menu-box">
+                <h1> User Approval Request <br/><br/>10</h1>
+              </div>
+              <div className="dashboard-menu-box">
+                <h1> Loan Approval Request <br/><br/>10 </h1>
+              </div>
+              <div className="dashboard-menu-box">
+                <h1> Total insurance <br/><br/>10</h1>
+              </div>
+              <div className="dashboard-menu-box">
+                <h1> Total Loan Approval <br/><br/>10</h1>
+              </div>
+              <div className="dashboard-menu-box">
+                <h1> Total Enquiry <br/><br/>10</h1>
+              </div>
+              <div className="dashboard-menu-box">
+                <h1> Total Car Insurance <br/><br/>10</h1>
+              </div>
+              <div className="dashboard-menu-box">
+                <h1> Total Cars <br/><br/>10</h1>
+              </div>
+            </div>
           </div>
         </div>
       </div>
