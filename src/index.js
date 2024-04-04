0329bccae8a77040aeb95aa5446042ef005a6c87
phpdev047 <phpdev047@gmail.com>
2024-04-04 17:45:25 +0530

    change

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import PrivacyPolicy from './Components/PrivacyPolicyOther';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
-  createBrowserRouter,
   BrowserRouter as Router,
-  RouterProvider,
   Routes,
   Route,
   Navigate
@@ -22,25 +20,30 @@ import AddCarComponent from './Components/AddCarComponent';
 import ApprovedUserComponent from './Components/ApprovedUserComponent';
 import FinancialLoanListing from './Components/FinancialLoanComponent';
 import ClaimLodgeListing from './Components/ClaimLodgeListingComponent';
-const App = createBrowserRouter([
-  {
-    path: "/",
-    element: <DashboardComponent />,
- 
-  children: [
-      {
-        path:"/approved-user",
-        element:<ApprovedUserComponent />
-      }
- 
-]
-},
-])
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<DashboardComponent />} />
+      <Route path="/privacy" element={<PrivacyPolicy />} />
+      <Route path="/approved-user" element={<ApprovedUserComponent />} />
+      <Route path="/dashboard" element={<DashboardComponent />} />
+      <Route path="/user" element={<UserComponent />} />
+      <Route path="/contact-us" element={<ContactUsComponent />} />
+      <Route path="/add-brand" element={<AddBrandComponent />} />
+      <Route path="/add-car" element={<AddCarComponent />} />
+      <Route path="/insurance-loan" element={<CarLoanListingComponent />} />
+      <Route path="/finance-loan" element={<FinancialLoanListing />} />
+      <Route path="/claim-lodge" element={<ClaimLodgeListing />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  </Router>
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <RouterProvider router={App} />
-</React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 reportWebVitals();
